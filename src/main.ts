571b994a4de3b5d8a9077995657b25ff7609fcb0
arsenijlevin <arsenij.levin19@gmail.com>
2023-03-19 19:22:00 +0300

    Инициализация работы swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API for IoT project')
+    .setDescription(
+      'Collecting temperature and humidity from HTTP request from STM32 microcontroller',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
